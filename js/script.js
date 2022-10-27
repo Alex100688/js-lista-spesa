827d663df13e9dce74e0556efdab2ab5734d791c
Alex100688 <alessiozingaro2@gmail.com>
2022-10-27 17:15:57 +0200

    Cambia l'ordine

--- js/script.js
+++ js/script.js
@@ -3,17 +3,16 @@
 let shoppingList=[`Vino`, `Pane`, `Borghetti`, `Pasta`, `Riso`];
 //faccio una querySelector del  id # list//
 const ulList=document.querySelector("list");
+const liList=document.createElement("li");
+//stampo il mio elemento con innerHtml//
 //faccio una variante impostando la i=0//
 let i = 0;
 //effetuo un ciclo while//
-while ( i<=shoppingList.length) {
-    console.log(i)
+while ( i<= shoppingList.length) {
+    console.log(shoppingList);
     i++;
+    liList.append(shoppingList[i])
+    console.log(shoppingList[i])
 }
 //creo un elemento(li)//
-const liList=document.createElement("li");
-//stampo il mio elemento con innerHtml//
-liList.innerHTML=shoppingList[i];
 //appendo la mia costante shoppinList alla costante ulList//
-ulList.append(liList);
-
