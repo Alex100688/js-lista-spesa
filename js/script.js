bad12897da1574253767437ae02140072088538d
Alex100688 <alessiozingaro2@gmail.com>
2022-10-27 17:33:58 +0200

    Aggiunge un append al ulList e stampa sul foglio

--- js/script.js
+++ js/script.js
@@ -2,17 +2,18 @@
 //creo una lista della spesa con array//
 let shoppingList=[`Vino`, `Pane`, `Borghetti`, `Pasta`, `Riso`];
 //faccio una querySelector del  id # list//
-const ulList=document.querySelector("list");
-const liList=document.createElement("li");
+const ulList=document.querySelector("#list");
 //stampo il mio elemento con innerHtml//
 //faccio una variante impostando la i=0//
 let i = 0;
 //effetuo un ciclo while//
-while ( i<= shoppingList.length) {
-    console.log(shoppingList);
+while ( i< shoppingList.length) {
+    const liList=document.createElement("li");
+    console.log(shoppingList[i]);
+    liList.innerHTML=shoppingList[i];
+    ulList.append(liList);
     i++;
-    liList.append(shoppingList[i])
-    console.log(shoppingList[i])
 }
+
 //creo un elemento(li)//
 //appendo la mia costante shoppinList alla costante ulList//
